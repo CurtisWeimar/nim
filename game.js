33104661b97de549d0e37b3d33a38a8939507482
Leonardo Contreras <LContreras@student.neumont.edu>
2021-07-15 18:36:11 -0600

    Merge branch 'main' of https://github.com/CurtisWeimar/nim into Leo

---
+++ game.js
@@ -0,0 +1,7 @@
+let difficultyChoice = sessionStorage.getItem("diff");
+let p1nameData = sessionStorage.getItem("p1name");
+let p2nameData = sessionStorage.getItem("p2name");
+
+console.log(difficultyChoice);
+console.log(p1nameData);
+console.log(p2nameData);
